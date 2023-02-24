ff344ed76177478cbdec2d07e5c1a469feba4da9
Gabriel Coelho <gabrielcuei98@gmail.com>
2023-02-24 19:35:13 +0000

    Remove unused API import and use axios directly in getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { type InferGetServerSidePropsType, type NextPage, type GetServerSideProps } from "next";
 
 import HomePage from "@/contents/HomePage";
-import { api } from "@/services/api";
+// import { api } from "@/services/api";
 import { type IProduct, type ICategories } from "@/interfaces";
+
+import axios from "axios";
 
 const Home: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ data }) => {
   return <HomePage data={data} />;
@@ -11,10 +13,12 @@ const Home: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = (
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const hostname = ctx.req.headers.host;
+
   const [categories, products] = await Promise.all([
-    api.get<ICategories[]>("/categories"),
-    api.get<IProduct[]>("/products"),
+    axios.get<ICategories[]>(`http://${hostname}/api/categories`),
+    axios.get<IProduct[]>(`http://${hostname}/api/products`),
   ]);
 
   const data = {
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const PORT = process.env.PORT || 3000;
-
 export const api = axios.create({
-  baseURL: `http://localhost:${PORT}/api`,
+  baseURL: `/api`,
 });
